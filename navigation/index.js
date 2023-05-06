8ac6b1ff574a3c1e4f488d9740fa85c56b6b40cc
Thế Vĩ <88361806+TheViHuynh@users.noreply.github.com>
2023-05-06 15:40:12 +0700

    merge 2 branches

--- navigation/index.js
+++ navigation/index.js
@@ -9,59 +9,62 @@ import UploadFaceScreen from "../screens/Login/UploadFaceScreen";
 import ActivityScreen from "../screens/Customer/Activity/ActivityScreen";
 import ActivityDetailScreen from "../screens/Customer/Activity/ActivityDetailScreen";
 import BookingScreen from "../screens/BookingScreen";
+import { NativeBaseProvider } from "native-base";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen
-          name="LoginHome"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignIn"
-          component={SignInScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Verify"
-          component={VerifyingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="UploadID"
-          component={UploadIDScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="UploadFace"
-          component={UploadFaceScreen}
-          options={{ headerShown: false }}
-        /> */}
-        {/* <Stack.Screen
-          name="Activity"
-          component={ActivityScreen}
-          options={{ headerShown: false }}
-        /> */}
-        {/* <Stack.Screen
-          name="Activity Detail"
-          component={ActivityDetailScreen}
-          options={{ headerShown: false }}
-        /> */}
-        <Stack.Screen
-          name="Booking"
-          component={BookingScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <Stack.Screen
+            name="LoginHome"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Verify"
+            component={VerifyingScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="UploadID"
+            component={UploadIDScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="UploadFace"
+            component={UploadFaceScreen}
+            options={{ headerShown: false }}
+          /> */}
+          {/* <Stack.Screen
+            name="Activity"
+            component={ActivityScreen}
+            options={{ headerShown: false }}
+          /> */}
+          {/* <Stack.Screen
+            name="Activity Detail"
+            component={ActivityDetailScreen}
+            options={{ headerShown: false }}
+          /> */}
+          <Stack.Screen
+            name="Booking"
+            component={BookingScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
