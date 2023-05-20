592480a856d5cf52a70bf879ff97f27faa71df2b
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2023-05-20 20:55:53 +0700

    fix sign up + handle event on card

--- screens/StudentOffice/StudentOfficeScreen.js
+++ screens/StudentOffice/StudentOfficeScreen.js
@@ -29,7 +29,8 @@ const StudentOfficeScreen = ({ navigation }) => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     getUsers();
-  }, []);
+    
+  }, [navigation]);
   const getUsers = () => {
     let users = [];
     getDocs(
